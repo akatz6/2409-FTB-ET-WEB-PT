ac502e522850440e3aa6e1e288c94f74829ffca4
akatz6 <aaronk488@gmail.com>
2024-12-12 08:00:13 -0800

    registerSlice

--- unit_three/block_27C/react_router/src/app/store.js
+++ unit_three/block_27C/react_router/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { api } from "./api";
+import registerSlice from "../components/Register/RegisterSlice";
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
+    register: registerSlice,
   },
   middleware: (getDefaultMiddleWare) =>
     getDefaultMiddleWare().concat(api.middleware),
